59de2c84e0b418f04855b123a29dc7aa5f99e34c
Alekseev Andrey <alekseev.andrew00@gmail.com>
2022-04-10 19:54:13 +0800

    Добавлена заглушка для репозитрия UserRepository

--- cmd/runTelegramBot.go
+++ cmd/runTelegramBot.go
@@ -1,10 +1,18 @@
 package main
 
 import (
-	"AskMeApp/internal/handlers"
+	"AskMeApp/internal/telegramBot"
+	"AskMeApp/internal/telegramBot/handlers"
+	"AskMeApp/repo"
+	"log"
 	"os"
 )
 
 func main() {
-	handlers.HandleBotMessages(os.Getenv("ASK_ME_APP_TG_TOKEN"))
+	botClient, err := telegramBot.NewBotClient(os.Getenv("ASK_ME_APP_TG_TOKEN"))
+	if err != nil {
+		log.Panic("Не удалось проинициализировать бота --- ", err)
+	}
+	userRepo := repo.NewUserRepository()
+	handlers.HandleBotMessages(botClient, userRepo)
 }
--- repo/userRepository.go
+++ repo/userRepository.go
@@ -1,6 +1,31 @@
 package repo
 
-//DbManager is structure for manage work with DB
-// TODO: реализовать основные методы из адаптера DbManagerInterface
-type DbManager struct {
+import "AskMeApp/internal/model"
+
+// UserRepository is structure for manage work with DB
+// TODO: реализовать основные методы из интерфейса interfaces.UserRepository
+type UserRepository struct {
+	Users []model.User
+}
+
+func NewUserRepository() *UserRepository {
+	return &UserRepository{
+		Users: make([]model.User, 5),
+	}
+}
+
+func (r UserRepository) GetByChatId(telegramChatId int64) (*model.User, error) {
+	return nil, nil
+}
+
+func (r UserRepository) Add(user *model.User) error {
+	return nil
+}
+
+func (r UserRepository) Delete(user *model.User) error {
+	return nil
+}
+
+func (r UserRepository) Edit(user *model.User) error {
+	return nil
 }
