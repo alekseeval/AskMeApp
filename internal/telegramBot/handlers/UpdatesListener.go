f690fd3bb45455eedf1034cd971bfee62bda8921
Alekseev Andrey <alekseev.andrew00@gmail.com>
2022-04-10 19:44:58 +0800

    Added User verification

--- internal/telegramBot/handlers/UpdatesListener.go
+++ internal/telegramBot/handlers/UpdatesListener.go
@@ -1,38 +1,70 @@
 package handlers
 
 import (
+	"AskMeApp/internal/interfaces"
+	"AskMeApp/internal/model"
 	"AskMeApp/internal/telegramBot"
+	TgBotApi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"log"
 )
 
-func HandleBotMessages(botClient *telegramBot.BotClient) {
+func HandleBotMessages(botClient *telegramBot.BotClient, userRepo interfaces.UserRepository) {
 	for update := range botClient.Updates {
 
 		if update.Message != nil {
-			chatId := update.Message.Chat.ID
+
+			user, err := VerifyOrRegisterUser(update.Message.Chat.ID, update.Message.From, userRepo)
+			if err != nil {
+				err = botClient.SendTextMessage("Что-то пошло не так: \n"+err.Error(), update.Message.Chat.ID)
+				if err != nil {
+					log.Panic("Жопа наступила, не удалось получить или создать юзера,"+
+						" а потом еще и сообщение не отправилось", err)
+				}
+			}
 
 			switch update.Message.Command() {
 			case "start":
-				err := botClient.SendMessage("Это была команда /start", chatId)
+				err = botClient.SendTextMessage("Это была команда /start", user.TgChatId)
 				if err != nil {
 					log.Panic("Не удалось отправить сообщение", err)
 				}
 			case "help":
-				err := botClient.SendMessage("Это была команда /help", chatId)
+				err = botClient.SendTextMessage("Это была команда /help", user.TgChatId)
 				if err != nil {
 					log.Panic("Не удалось отправить сообщение", err)
 				}
 			case "question":
-				err := botClient.SendMessage("Это была команда /question", chatId)
+				err = botClient.SendTextMessage("Это была команда /question", user.TgChatId)
 				if err != nil {
 					log.Panic("Не удалось отправить сообщение", err)
 				}
 			case "changecategory":
-				err := botClient.SendMessage("Это была команда /changecategory", chatId)
+				err = botClient.SendTextMessage("Это была команда /changecategory", user.TgChatId)
 				if err != nil {
 					log.Panic("Не удалось отправить сообщение", err)
 				}
 			}
 		}
 	}
+}
+
+func VerifyOrRegisterUser(tgChatId int64, tgUserInfo *TgBotApi.User, repository interfaces.UserRepository) (*model.User, error) {
+	user, err := repository.GetByChatId(tgChatId)
+	if err != nil {
+		return nil, err
+	}
+	if user != nil {
+		return user, nil
+	}
+	user = &model.User{
+		FirstName:  tgUserInfo.FirstName,
+		LastName:   tgUserInfo.LastName,
+		TgUserName: tgUserInfo.UserName,
+		TgChatId:   tgChatId,
+	}
+	err = repository.Add(user)
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
 }
