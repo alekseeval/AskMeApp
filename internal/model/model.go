eac2d701b34b9434ca46284147b0c58442abc7c6
Alekseev Andrey <alekseev.andrew00@gmail.com>
2022-04-22 09:30:11 +0800

    Added Category and Question structs

--- internal/model/model.go
+++ internal/model/model.go
@@ -1,9 +1,25 @@
 package model
 
 type User struct {
-	Id         int32  `json:"id"`
-	FirstName  string `json:"firstName"`
-	LastName   string `json:"lastName"`
+	Id int32 `json:"id"`
+
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+
 	TgChatId   int64  `json:"tgChatId"`
 	TgUserName string `json:"tgUserName"`
+}
+
+type Category struct {
+	Id    int32
+	Title string
+}
+
+type Question struct {
+	Id     int32  `json:"id"`
+	Title  string `json:"title"`
+	Answer string `json:"answer"`
+
+	Category *Category `json:"category"`
+	AuthorId *User     `json:"authorId"`
 }
