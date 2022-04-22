d0d29c5386c1568ee13ffb5a466ebf82d13e34fa
Alekseev Andrey <alekseev.andrew00@gmail.com>
2022-04-22 09:46:39 +0800

    Added QuestionRepository base template

--- repo/questionRepository.go
+++ repo/questionRepository.go
@@ -1,1 +1,26 @@
 package repo
+
+import (
+	"AskMeApp/internal/model"
+	"database/sql"
+)
+
+type QuestionRepository struct {
+	db *sql.DB
+}
+
+func (repo *QuestionRepository) Add(question *model.Question) (*model.Question, error) {
+	return nil, nil
+}
+
+func (repo *QuestionRepository) Delete(question *model.Question) error {
+	return nil
+}
+
+func (repo *QuestionRepository) Edit(question *model.Question) error {
+	return nil
+}
+
+func (repo *QuestionRepository) GetByUser(user *model.User) (*model.Question, error) {
+	return nil, nil
+}
