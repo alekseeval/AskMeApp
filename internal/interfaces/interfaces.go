9c6c0f172d4d8c5a4467e0fabfc054a3be1d8de5
Alekseev Andrey <alekseev.andrew00@gmail.com>
2022-04-22 09:36:22 +0800

    Added QuestionRepositoryInterface

--- internal/interfaces/interfaces.go
+++ internal/interfaces/interfaces.go
@@ -7,9 +7,15 @@ type UserRepositoryInterface interface {
 	Add(user *model.User) (*model.User, error)
 	Delete(user *model.User) error
 	Edit(user *model.User) error
+
 	GetByChatId(telegramChatId int64) (*model.User, error)
 }
 
 // QuestionsRepositoryInterface is CRUD repository interface for model.Question
 type QuestionsRepositoryInterface interface {
+	Add(question *model.Question) (*model.Question, error)
+	Delete(question *model.Question) error
+	Edit(question *model.Question) error
+
+	GetByUser(user *model.User) (*model.Question, error)
 }
