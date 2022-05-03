b38dc70723525512fb1783a6872abe9e0fe919e0
Alekseev Andrey <alekseev.andrew00@gmail.com>
2022-05-04 03:21:35 +0800

    Fixed convertion from int32 to string

--- internal/telegramBot/client/messagesMethods.go
+++ internal/telegramBot/client/messagesMethods.go
@@ -2,6 +2,7 @@ package client
 
 import (
 	"AskMeApp/internal/model"
+	"fmt"
 	TgBotApi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
@@ -17,7 +18,7 @@ func (bot *BotClient) SendTextMessage(msgText string, chatId int64) error {
 func (bot *BotClient) SendInlineCategories(messageExplanation string, categories []*model.Category, numberInRow int, chatId int64) error {
 	inlineButtons := make([][]TgBotApi.InlineKeyboardButton, (len(categories)-1)/numberInRow+1)
 	for i, category := range categories {
-		inlineButtons[i/numberInRow] = append(inlineButtons[i/numberInRow], TgBotApi.NewInlineKeyboardButtonData(category.Title, string(category.Id)))
+		inlineButtons[i/numberInRow] = append(inlineButtons[i/numberInRow], TgBotApi.NewInlineKeyboardButtonData(category.Title, fmt.Sprint(category.Id)))
 	}
 	inlineMarkup := TgBotApi.NewInlineKeyboardMarkup(inlineButtons...)
 	msg := TgBotApi.NewMessage(chatId, messageExplanation)
