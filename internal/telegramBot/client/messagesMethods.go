2f8ef915a8c7d64c0fd0b7ca8a260cc1939347a2
Alekseev Andrey <alekseev.andrew00@gmail.com>
2022-05-04 03:06:08 +0800

    Added method SendInlineCategories

--- internal/telegramBot/client/messagesMethods.go
+++ internal/telegramBot/client/messagesMethods.go
@@ -1,6 +1,9 @@
 package client
 
-import TgBotApi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+import (
+	"AskMeApp/internal/model"
+	TgBotApi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
 
 func (bot *BotClient) SendTextMessage(msgText string, chatId int64) error {
 	msg := TgBotApi.NewMessage(chatId, msgText)
@@ -9,4 +12,16 @@ func (bot *BotClient) SendTextMessage(msgText string, chatId int64) error {
 		return err
 	}
 	return nil
+}
+
+func (bot *BotClient) SendInlineCategories(messageExplanation string, categories []*model.Category, numberInRow int, chatId int64) error {
+	inlineButtons := make([][]TgBotApi.InlineKeyboardButton, (len(categories)-1)/numberInRow+1)
+	for i, category := range categories {
+		inlineButtons[i/numberInRow] = append(inlineButtons[i/numberInRow], TgBotApi.NewInlineKeyboardButtonData(category.Title, string(category.Id)))
+	}
+	inlineMarkup := TgBotApi.NewInlineKeyboardMarkup(inlineButtons...)
+	msg := TgBotApi.NewMessage(chatId, messageExplanation)
+	msg.ReplyMarkup = inlineMarkup
+	_, err := bot.bot.Send(msg)
+	return err
 }
