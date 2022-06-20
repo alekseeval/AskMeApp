f6a01883e56b023e27ec6fac4a9c69567a2e2bfd
Andrey Alekseev <a.alekseev@rosinnova.com>
2022-06-20 18:37:49 +0800

    Изменен метод выдачи случайного вопроса. Теперь выдается вопрос соответствующий выбранной категории

--- internal/telegramBot/messageMethods.go
+++ internal/telegramBot/messageMethods.go
@@ -2,6 +2,7 @@ package telegramBot
 
 import (
 	"AskMeApp/internal"
+	"errors"
 	"fmt"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"sort"
@@ -84,8 +85,31 @@ func (botClient *BotClient) SendRandomQuestionToUser(user *internal.User) error 
 		}
 		return nil
 	}
-	question := GetRandomQuestion(allQuestions)
 
+	currentUserState, ok := botClient.usersStates[user.TgChatId]
+	if !ok {
+		return errors.New("user have no current state")
+	}
+	requestedQuestions := make([]*internal.Question, 0)
+	for _, question := range allQuestions {
+		for _, category := range question.Categories {
+			if category.Id == currentUserState.CurrentCategory.Id {
+				requestedQuestions = append(requestedQuestions, question)
+				continue
+			}
+		}
+	}
+	if len(requestedQuestions) == 0 {
+		msg := tgbotapi.NewMessage(user.TgChatId, "На данный момент вопросы по категории __"+currentUserState.CurrentCategory.Title+"__ отсутствуют")
+		msg.ParseMode = "MarkdownV2"
+		_, err = botClient.bot.Send(msg)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+
+	question := GetRandomQuestion(requestedQuestions)
 	themesText := ""
 	for _, category := range question.Categories {
 		themesText += "\t__" + category.Title + "__"
