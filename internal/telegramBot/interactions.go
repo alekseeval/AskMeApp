cfbd8538c4a481be23bf7eddb9eb9b652159bb6c
Alekseev Andrey <alekseev.andrew00@gmail.com>
2022-06-22 22:54:23 +0800

    Реализовано добавление вопроса в базу

--- internal/telegramBot/interactions.go
+++ internal/telegramBot/interactions.go
@@ -43,8 +43,9 @@ func NewUserState(currentCategory internal.Category) *userState {
 
 func (state *userState) increaseStep() *userState {
 	state.SequenceStep -= totalNumberOfScenarios + 1
-	if state.SequenceStep < 0 {
+	if state.SequenceStep <= 0 {
 		state.SequenceStep = nilStep
+		state.unfilledNewQuestion = nil
 	}
 	return state
 }
@@ -97,7 +98,76 @@ func (botClient *BotClient) ProcessUserStep(user *internal.User, userState *user
 			return userState, err
 		}
 
-	//	TODO: Question steps
+	case NewQuestionStartStep:
+		msg := tgbotapi.NewMessage(user.TgChatId, "Enter your question:")
+		msg.ParseMode = "MarkdownV2"
+		_, err := botClient.botApi.Send(msg)
+		if err != nil {
+			return userState, err
+		}
+		userState.unfilledNewQuestion = &internal.Question{
+			Author: user,
+		}
+	case newQuestion3Step:
+		if update.Message.Text == "" {
+			return userState, errors.New("empty question title received")
+		}
+		userState.unfilledNewQuestion.Title = update.Message.Text
+		msg := tgbotapi.NewMessage(user.TgChatId, "Enter answer of your question:")
+		msg.ParseMode = "MarkdownV2"
+		_, err := botClient.botApi.Send(msg)
+		if err != nil {
+			return userState, err
+		}
+	case newQuestion2Step:
+		if update.Message.Text == "" {
+			return userState, errors.New("empty question title received")
+		}
+		userState.unfilledNewQuestion.Answer = update.Message.Text
+		allCategories, err := botClient.questionRepository.GetAllCategories()
+		if err != nil {
+			return userState, err
+		}
+		err = botClient.SendCategoriesToChooseInline(
+			"Choose category of your question:", allCategories, user.TgChatId)
+		if err != nil {
+			return userState, err
+		}
+	case newQuestionEndStep:
+		callbackData := update.CallbackData()
+		if callbackData == "" || callbackData[0] != 'c' {
+			allCategories, err := botClient.questionRepository.GetAllCategories()
+			if err != nil {
+				return userState, err
+			}
+			err = botClient.SendCategoriesToChooseInline(
+				"Again. Choose category of your question:", allCategories, user.TgChatId)
+			if err != nil {
+				return userState, err
+			}
+			return userState, nil
+		}
+		categoryId, err := strconv.ParseInt(callbackData[1:len(callbackData)], 10, 32)
+		if err != nil {
+			return userState, nil
+		}
+		category, err := botClient.questionRepository.GetCategoryById(int32(categoryId))
+		if err != nil {
+			return userState, err
+		}
+		userState.unfilledNewQuestion.Categories = make([]*internal.Category, 0)
+		userState.unfilledNewQuestion.Categories = append(userState.unfilledNewQuestion.Categories, category)
+		if category.Id != baseCategory.Id {
+			userState.unfilledNewQuestion.Categories = append(userState.unfilledNewQuestion.Categories, &baseCategory)
+		}
+		_, err = botClient.questionRepository.AddQuestion(userState.unfilledNewQuestion)
+		if err != nil {
+			return userState, err
+		}
+		callback := tgbotapi.NewCallback(update.CallbackQuery.ID, "Question successfully saved!")
+		if _, err := botClient.botApi.Request(callback); err != nil {
+			return nil, err
+		}
 	default:
 		return userState, errors.New("unknown step")
 	}
